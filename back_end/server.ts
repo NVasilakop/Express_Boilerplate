5445fbe858688d9fd211f12c0cf33452d51509c4
Nick Vasilakopoulos <nvasilakop@gmail.com>
2021-10-01 17:27:12 +0300

    Add small things in package.json

--- back_end/server.ts
+++ back_end/server.ts
@@ -5,15 +5,25 @@
  import express from "express";
  import path from "path";
  import { Request, Response } from 'express';
+ import cors from "cors";
+ import helmet from "helmet";
+ import morgan, { StreamOptions } from "morgan";
 /**
  * App Variables
  */
  const app = express();
  const port = process.env.PORT || "8000";
+//  const stream: StreamOptions = {
+//   // Use the http severity
+//   write: (message) => Logger.http(message),
+// };
 /**
  *  App Configuration
  */
-
+ app.use(helmet());
+ app.use(cors);
+ app.use(express.json())
+ app.use(morgan('combined'));
 /**
  * Routes Definitions
  */
